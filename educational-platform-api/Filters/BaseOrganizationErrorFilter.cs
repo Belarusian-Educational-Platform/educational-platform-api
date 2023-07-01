4a4df86709801c8f665e9bf6b732f8174976c96a
Discipuls <ivan.hontarau@gmail.com>
2023-07-01 18:11:42 +0300

    Implement base error classes and error filters

---
+++ educational-platform-api/Filters/BaseOrganizationErrorFilter.cs
@@ -0,0 +1,20 @@
+﻿using educational_platform_api.Exceptions;
+using HotChocolate;
+
+namespace educational_platform_api.Filters
+{
+    public class BaseOrganizationErrorFilter : IErrorFilter
+    {
+        public IError OnError(IError error)
+        {
+            if (typeof(BaseOrganizationException).IsAssignableFrom(error.Exception.GetType()))
+            {
+                return error.WithMessage("Exception inhearts BaseOrganizationException");
+            }
+            else
+            {
+                return error;
+            }
+        }
+    }
+}
