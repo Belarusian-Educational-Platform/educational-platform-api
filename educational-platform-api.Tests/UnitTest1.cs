ce5e16471c2d5015a47962b626d8432b24dc70a9
Kononenko-Daniil <neokv210@gmail.com>
2023-07-19 15:28:15 +0300

    Add MSTest project

---
+++ educational-platform-api.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace educational_platform_api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
