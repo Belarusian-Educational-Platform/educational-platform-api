bac185ac05ceef21830c7b8dbbafc4ff07678151
Discipuls <87646124+Discipuls@users.noreply.github.com>
2023-08-19 19:59:39 +0300

    Uncompleted commit 2

--- educational-platform-api/DTOs/Organization/UpdateOrganizationInput.cs
+++ educational-platform-api/DTOs/Organization/UpdateOrganizationInput.cs
@@ -3,9 +3,9 @@
     public class UpdateOrganizationInput
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
     }
 }
